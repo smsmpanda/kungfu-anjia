e6fa9028a1c0e71672b7138647f045b5d5b2c801
smsmpandas <smsmpandas@163.com>
2023-04-05 22:45:07 +0800

    vue使用方式修正

--- src/views/layout/login.vue
+++ src/views/layout/login.vue
@@ -5,13 +5,16 @@
       <el-main>
         <el-card class="login-box-wrap">
           <div class="login-item">
-            <el-input v-model="account" class="w-50 m-2" size="large" placeholder="账号名/邮箱/手机" :prefix-icon="User" />
+            <el-input v-model="data.account" class="w-50 m-2" size="large" placeholder="账号名/邮箱/手机" :prefix-icon="User" />
           </div>
           <div class="login-item">
-            <el-input v-model="password" type="password" class="w-50 m-2" size="large" placeholder="请输入登录密码" :prefix-icon="Key" />
+            <el-input v-model="data.password" type="password" class="w-50 m-2" size="large" placeholder="请输入登录密码"
+              :prefix-icon="Key" />
           </div>
           <div class="login-item">
-            <el-button class="login-btn" type="primary" :loading-icon="Eleme" :loading="loading" @click="login">{{ loginBtnContent }}</el-button>
+            <el-button class="login-btn" type="primary" :loading-icon="Eleme" :loading="data.loginBtnIsShowLoading"
+              @click="login">{{
+                data.loginBtnContent }}</el-button>
           </div>
         </el-card>
       </el-main>
@@ -20,35 +23,34 @@
 </template>
 
 <script>
-import { Calendar, Search, User, Loading,Key,Eleme  } from '@element-plus/icons-vue';
+
+import { useRouter } from 'vue-router'
+import { reactive } from 'vue'
+import { Calendar, Search, User, Loading, Key, Eleme } from '@element-plus/icons-vue'
+
 export default {
+  name: 'login',
   setup() {
-    return {
-      Search,
-      User,
-      Calendar,
-      Loading,
-      Key,
-      Eleme
-    }
-  },
-  data() {
-    return {
+    const router = useRouter()
+    let data = reactive({
       account: 'test',
       password: 'password',
-      loginBtnContent:'登录',
-      loading:false
-    };
-  },
-  methods: {
-    login(){
-      this.loading = true;
-      this.loginBtnContent = '登录中...'
-      setTimeout(() => {
-        this.$router.push('/home');
-      }, 2000);
+      loginBtnContent: '登录',
+      loginBtnIsShowLoading: false,
+    })
+
+    function login() {
+      data.loginBtnIsShowLoading = true;
+      data.loginBtnContent = '登录中...';
+      router.push('/home');
     }
-  },
+
+    return {
+      Calendar, Search, User, Loading, Key, Eleme,
+      data,
+      login
+    }
+  }
 };
 </script>
 
@@ -82,8 +84,8 @@ export default {
   margin: 20px 10px;
 }
 
-.login-btn{
-  width:100%;
+.login-btn {
+  width: 100%;
   height: 40px
 }
 </style>

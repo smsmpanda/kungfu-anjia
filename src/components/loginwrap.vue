6491a0a26782ddcc2abb419cc06f9a67d5cd50cd
smsmpandas <smsmpandas@163.com>
2023-05-22 08:30:20 +0800

    revert homepage

--- src/components/loginwrap.vue
+++ src/components/loginwrap.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-        <div>{{ loginOption }}</div>
-        <slot name="content">
-
-        </slot>
+    <div class="ym-login-wrap">
+        <div class="ym-login-nav">
+            <div class="ym-login-title">{{ loginOption }}</div>
+            <div class="ym-login-subtitle ">欢迎来到小窝坊</div>
+        </div>
+        <div class="ym-login-contentwarp">
+            <slot name="content">
+            </slot>
+        </div>
+        <div class="ym-login-footer">
+            <slot name="foot"></slot>
+        </div>
     </div>
 </template>
 
@@ -14,7 +21,62 @@ export default {
         loginOption: String
     },
     setup() {
+        function switchLoginMethod() {
 
+        }
+        return {
+            switchLoginMethod
+        }
     }
 }
 </script>
+<style scoped>
+.ym-login-wrap {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+}
+
+.ym-login-title {
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 24px;
+    color: #10141A;
+    text-align: center;
+}
+
+.ym-login-subtitle {
+    font-size: 12px;
+    line-height: 16px;
+    color: rgba(11, 18, 26, 0.6);
+    text-align: center;
+    margin-top: 8px;
+    margin-bottom: 32px;
+}
+
+.ym-login-nav {
+    color: #10141A;
+    text-align: center;
+    padding-top: 40px;
+}
+
+.ym-login-footer {
+    padding: 16px 0 17px;
+    border-top: solid 1px #EBEFF2;
+    font-size: 12px;
+    line-height: 16px;
+    color: rgba(11, 18, 26, 0.6);
+    text-align: center;
+}
+
+.ym-login-contentwarp {
+    flex: 1;
+}
+
+.ym-login-other {
+    text-decoration: none;
+}
+</style>
--- src/views/layout/login.vue
+++ src/views/layout/login.vue
@@ -1,41 +1,57 @@
 <template>
-  <div style="background-color: var(--ym-primary);" class="common-layout ym-height-fill">
+  <div class="common-layout ym-height-fill">
     <el-container class="ym-height-fill">
       <el-header class="login-header">
         <logo style="width:80px;top:10px" :logoWidth="50" :logoHeight="50" />
       </el-header>
       <el-main>
-        <el-card class="login-box-wrap">
-          <el-row>
-            <el-col :span="12">
-              <el-image style="width:auto;height: 100%;" :src="data.loginImg"></el-image>
-            </el-col>
-            <el-col :span="12" style="position:relative">
-              <div class="login-module-box">
-                <loginwrap loginOption="微信">
-                  <template #content>
-                    <div class="login-item">
-                      <el-input v-model="data.account" class="login-input w-50 m-2" size="large" placeholder="账号名/邮箱/手机"
-                        :prefix-icon="User" />
-                    </div>
-                    <div class="login-item">
-                      <el-input v-model="data.password" type="password" class="login-input w-50 m-2" size="large"
-                        placeholder="请输入登录密码" :prefix-icon="Key" />
-                    </div>
-                    <div class="login-item">
-                      <el-button class="login-input login-btn" type="primary" :loading-icon="Eleme"
-                        :loading="data.loginBtnIsShowLoading" @click="login">{{
-                          data.loginBtnContent }}</el-button>
-                    </div>
-                    <div class="login-item">
-                      <router-link to="/resetpwd">找回密码</router-link> | <router-link to="/register">快速注册</router-link>
-                    </div>
-                  </template>
-                </loginwrap>
+        <div class="login-box-wrap">
+          <loginwrap loginOption="账号密码登录">
+            <template #content>
+              <div class="login-item">
+                <el-input v-model="data.account" class="login-input w-50 m-2" size="large" placeholder="账号名/邮箱/手机"
+                  :prefix-icon="User" />
               </div>
-            </el-col>
-          </el-row>
-        </el-card>
+              <div class="login-item">
+                <el-input v-model="data.password" type="password" class="login-input w-50 m-2" size="large"
+                  placeholder="请输入登录密码" :prefix-icon="Key" />
+              </div>
+              <div class="login-item">
+                <el-button class="login-input login-btn" type="primary" :loading-icon="Eleme"
+                  :loading="data.loginBtnIsShowLoading" @click="login">{{
+                    data.loginBtnContent }}</el-button>
+              </div>
+              <div class="login-item">
+                <!-- <router-link to="/resetpwd">找回密码</router-link> | <router-link to="/register">快速注册</router-link> -->
+              </div>
+            </template>
+            <template #foot>
+              未开通小窝坊帐号可通过
+              <a class="ym-login-mehtod-item" href="" @click.prevent="switchOthenLoginMethod">其他方式登录</a>
+            </template>
+          </loginwrap>
+          <loginwrap loginOption="微信扫码登录" class="login-wrap-hide" :class="{ 'login-slide-show': data.loginMethod.other }">
+            <template #content>
+              <div class="login-qrcode">
+                <div class="login-qrcode-wrap">
+                  <el-image :src="data.loginEwm" fit="cover" style="width:150px; height:150px" />
+                </div>
+              </div>
+            </template>
+            <template #foot>
+              <div class="ym-login-methods">
+                <a class="ym-login-mehtod-item no-select" href="" @click.prevent="switchOthenLoginMethod">
+                  <el-icon>
+                    <ArrowLeft />
+                  </el-icon>
+                  账号密码
+                </a>
+                <a class="ym-login-mehtod-item no-select" href="" @click.prevent="switchLmWx">微信</a>
+                <a class="ym-login-mehtod-item no-select" href="" @click.prevent="switchLmPhone">手机号</a>
+              </div>
+            </template>
+          </loginwrap>
+        </div>
       </el-main>
       <el-footer>
         <div class="login-footer"></div>
@@ -48,15 +64,16 @@
 
 import { useRouter } from 'vue-router'
 import { reactive } from 'vue'
-import { Calendar, Search, User, Loading, Key, Eleme } from '@element-plus/icons-vue'
+import { Calendar, Search, User, Loading, Key, Eleme, ArrowLeft } from '@element-plus/icons-vue'
 import loginwrap from '~/components/loginwrap.vue'
-import loginImg from '~/assets/logo-main.png'
+import loginEwm from '~/assets/ewm.png'
 import logo from '~/components/logo.vue'
 
 export default {
   name: 'login',
   components: {
-    logo
+    logo,
+    ArrowLeft
   },
   setup() {
     const router = useRouter()
@@ -65,13 +82,20 @@ export default {
       password: 'password',
       loginBtnContent: '登录',
       loginBtnIsShowLoading: false,
-      loginImg: loginImg
+      loginEwm: loginEwm,
+      loginMethod: {
+        other: false
+      }
     })
 
     function login() {
       data.loginBtnIsShowLoading = true;
       data.loginBtnContent = '登录中...';
       router.push('/home');
+    }
+
+    function switchOthenLoginMethod() {
+      data.loginMethod.other = !data.loginMethod.other
     }
 
     return {
@@ -82,10 +106,12 @@ export default {
       Loading,
       Key,
       Eleme,
+      ArrowLeft,
       //data
       data,
       //methods
-      login
+      login,
+      switchOthenLoginMethod
     }
   }
 };
@@ -105,9 +131,14 @@ export default {
   position: absolute;
   top: 50%;
   left: 50%;
-  width: 85%;
+  width: 340px;
+  height: 415px;
   transform: translate(-50%, -50%);
+  background: #FFF;
+  overflow: hidden;
 }
+
+
 
 .ym-login-footer {
   text-align: center;
@@ -135,13 +166,56 @@ export default {
   font-size: 12px;
 }
 
-.login-module-box {
+.ym-login-methods {
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+}
+
+.ym-login-methods>a {
+  position: relative;
+  width: 33%;
+}
+
+.ym-login-methods :not(:first-child):before {
+  content: '';
+  height: 13px;
+  width: 1px;
+  border-radius: 10px;
+  display: inline-block;
+  background-color: rgb(238, 238, 238);
   position: absolute;
-  width: 340px;
-  height: auto;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #ffffff;
+  left: 0;
+  top: 1px;
+}
+
+
+.ym-login-mehtod-item {
+  text-decoration: none;
+}
+
+.no-select {
+  color: #7a7777;
+}
+
+.login-wrap-hide {
+  left: 100%;
+  transition: all .2s ease-in-out;
+}
+
+.login-slide-show {
+  left: 0;
+}
+
+.login-qrcode {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.login-qrcode-wrap {
+  padding: 10px;
+  border: 1px solid #f1eeee;
+  border-radius: 5px;
 }
 </style>
